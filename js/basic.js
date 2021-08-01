4d762278fc59dfb032988ddc729f86f9c868b570
jangbly62 <wkdgur112@gmail.com>
2021-08-01 17:23:43 +0900

    order change

--- js/basic.js
+++ js/basic.js
@@ -1,14 +1,18 @@
-const btn_time = document.querySelector("#order_launch button");
-const btn_launch = document.querySelector("#order_time button");
+const btn_time = document.querySelector("#btn_time");
+const btn_launch = document.querySelector("#btn_launch");
 const panel_main = document.querySelector("body>div.order");
 
 function screenMoveToRight() {
   panel_main.classList.add("moveToRight");
   panel_main.classList.remove("moveToLeft");
+  btn_time.classList.add("currentOrder");
+  btn_launch.classList.remove("currentOrder");
 }
 function screenMoveToLeft() {
   panel_main.classList.remove("moveToRight");
   panel_main.classList.add("moveToLeft");
+  btn_time.classList.remove("currentOrder");
+  btn_launch.classList.add("currentOrder");
 }
 
 btn_time.addEventListener("click", screenMoveToRight);
