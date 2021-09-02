a731701ac746fb0b71166610b2dc75105f58f10a
jangbly62 <wkdgur112@gmail.com>
2021-09-02 18:26:44 +0900

    js distribution

--- js/basic.js
+++ js/basic.js
@@ -2,6 +2,53 @@ const btn_sort = document.querySelector("#btn_sort");
 const sort_href = ["sort_movie.html", "sort_character.html", "sort_stone.html"];
 const sort_button = ["영화", "캐릭터", "스톤"];
 
+// 함수 생성
+function f_div_movie(div_movie) {
+  div_movie.classList.add("eachMovie");
+  div_movie.classList.add("movie_div_size");
+  div_movie.style.display = "flex";
+  div_movie.style.flexDirection = "column";
+  div_movie.style.alignItems = "center";
+  div_movie.style.paddingBottom = "10px";
+}
+
+function f_img_movie(img_movie, i, array) {
+  img_movie.src = array[i].img;
+  img_movie.classList.add("movie_img_size");
+  img_movie.alt = "no image";
+}
+
+function f_a_title(a_title) {
+  a_title.rel = "noreferrer";
+  a_title.target = "_blank";
+  a_title.style.textAlign = "center";
+}
+
+function createDivEach(array, index, balloon = true) {
+  const div_movie = document.createElement("div");
+  const div_title = document.createElement("div");
+  const img_movie = document.createElement("img");
+  const a_img = document.createElement("a");
+  const a_title = document.createElement("a");
+  const returnArray = [div_movie, a_img, a_title];
+
+  f_div_movie(div_movie);
+  f_img_movie(img_movie, index, array);
+
+  a_title.innerText = `${array[index].name_kr}(${array[index].name_en}) - ${array[index].launch_year}`;
+
+  a_img.appendChild(img_movie);
+  div_movie.appendChild(a_img);
+  div_title.appendChild(a_title);
+  div_movie.appendChild(div_title);
+  if (balloon === true) {
+    div_movie.addEventListener("mouseenter", balloonMenuEachMovie);
+    div_movie.addEventListener("mouseleave", removeBalloonMenuEachMovie);
+  }
+
+  return returnArray;
+}
+
 function titleAnchorHref(a, i) {
   a.href = sort_href[i];
 }
---
+++ js/main.js
@@ -0,0 +1,51 @@
+// 시간순 div 지정
+const order_time = document.querySelector(
+  "#order_time .phase:first-child .phase_movie"
+);
+
+// 개봉예정 div 지정
+const order_schedule = document.querySelector(
+  "#order_schedule .phase:first-child .phase_movie"
+);
+
+// 페이즈 div 지정
+const order_launch_p = [
+  document.querySelector("#order_launch .phase:first-child .phase_movie"),
+  document.querySelector("#order_launch .phase:nth-child(2) .phase_movie"),
+  document.querySelector("#order_launch .phase:nth-child(3) .phase_movie"),
+  document.querySelector("#order_launch .phase:nth-child(4) .phase_movie"),
+];
+
+// 개봉순 정렬
+for (let p = 0; p < 4; p++) {
+  if (order_launch_p[p] != null) {
+    for (let i = 0; i < movies_launch.length; i++) {
+      const return_launch = createDivEach(movies_launch, i);
+      f_a_title(return_launch[1], i, movies_launch);
+      f_a_title(return_launch[2], i, movies_launch);
+      if (String(p + 1) === movies_launch[i].phase) {
+        order_launch_p[p].appendChild(return_launch[0]);
+      }
+    }
+  }
+}
+
+// 타임라인순 정렬
+for (let il = 0; il < movies_time.length; il++) {
+  if (order_time != null) {
+    const return_time = createDivEach(movies_time, il);
+    f_a_title(return_time[1], il, movies_time);
+    f_a_title(return_time[2], il, movies_time);
+    order_time.appendChild(return_time[0]);
+  }
+}
+
+// 개봉예정작
+for (let il = 0; il < movies_schedule.length; il++) {
+  if (order_schedule != null) {
+    const return_schedule = createDivEach(movies_schedule, il);
+    f_a_title(return_schedule[1], il, movies_schedule);
+    f_a_title(return_schedule[2], il, movies_schedule);
+    order_schedule.appendChild(return_schedule[0]);
+  }
+}
